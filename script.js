7ee2c0341aaac655b6599d61766e4911c942b952
Megan Murphy <childovhurin@gmail.com>
2020-05-16 19:50:16 -0400

    js variables and API key

---
+++ script.js
@@ -0,0 +1,8 @@
+//VARIABLES
+var citySearch;
+var cityLat;
+var cityLon;
+
+function makeForecastAPICall() {
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "e6fe4067ecf4ac4d661a51ad107a4684";
+}
